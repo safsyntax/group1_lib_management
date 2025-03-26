adfe7fc962b8132c391ba3db96ac9ef7f7d6b242
bhargav-rishi <rishibhargav.m@gmail.com>
2025-03-26 11:46:29 -0400

    Modified Book.cs

--- Book.cs
+++ Book.cs
@@ -1,25 +1,25 @@
 ﻿public class Book
 {
-	private string _title;
-	private string _author;
-	private string _isbn;
-	private int _availablecopies;
-	public Book(string title, string author, string isbn, int availablecopies)
-	{
-		_title = title;
-		_author = author;
-		_isbn = isbn;
-		_availablecopies = availablecopies;
-	}
-	public bool BorrowBook(string title)
-	{
-		if (_title = title && _availablecopies>0)
-		{
-			_availablecopies = _availablecopies - 1;
-			return true;
-		}
-		return false;
-	}
+    public string _title;
+    public string _author;
+    public string _isbn;
+    public int _availablecopies;
+    public Book(string title, string author, string isbn, int availablecopies)
+    {
+        _title = title;
+        _author = author;
+        _isbn = isbn;
+        _availablecopies = availablecopies;
+    }
+    public bool BorrowBook(string title)
+    {
+        if (_title == title && _availablecopies > 0)
+        {
+            _availablecopies = _availablecopies - 1;
+            return true;
+        }
+        return false;
+    }
     public void DisplayBook()
     {
         Console.WriteLine($"Title: {_title}, Author: {_author}, Available Copies: {_availablecopies}");
