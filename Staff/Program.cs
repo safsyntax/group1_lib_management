9d64aa0db5b7aa8482a07d83ebd9605b8b3e2d18
safsyntax <safiyajoseph@usf.edu>
2025-03-26 11:34:57 -0400

    adding staff file

---
+++ Staff/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+public class Staff : Person
+{
+    public string Position;
+    public string Department;
+ 
+    public Staff(string name, string email, string id, string position, string department)
+        : base(name, email, id)
+    {
+        Position = position;
+        Department = department;
+    }
+}
